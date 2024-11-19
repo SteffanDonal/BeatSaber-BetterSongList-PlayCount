0fb8a5c68099ed2c7d1857c014dc1986d3c580da
SteffanDonal <steffan@donal.family>
2024-11-19 16:51:18 +0000

    Add plugin files, MVP release!

---
+++ PlayCountSorter.cs
@@ -0,0 +1,79 @@
+﻿using BetterSongList.Interfaces;
+using BetterSongList.SortModels;
+using BetterSongList.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlayCount
+{
+    public class PlayCountSorter : ISorterPrimitive, ISorterWithLegend, ITransformerPlugin
+    {
+        static PlayerDataModel playerDataModel;
+
+        public bool isReady => true;
+
+        public string name => "Play Count";
+
+        public bool visible => true;
+
+        public IEnumerable<KeyValuePair<string, int>> BuildLegend(BeatmapLevel[] levels)
+            => SongListLegendBuilder.BuildFor(levels, level => GetGroupName(CalculateGroupNumberForPlays(GetPlayCountFor(level))));
+
+        string GetGroupName(int group)
+        {
+            if (group == 0)
+                return "Never";
+            else if (group < 10)
+                return $"{group}x";
+            else
+                return $"{group}+";
+        }
+        int CalculateGroupNumberForPlays(int numPlays)
+        {
+            if (numPlays < 10)
+                return numPlays;
+
+            var groupScale = (int)Math.Pow(10, numPlays.ToString().Length - 1);
+            var scaleMultiplier = numPlays / groupScale;
+
+            return groupScale * scaleMultiplier;
+        }
+
+        public float? GetValueFor(BeatmapLevel song) => GetPlayCountFor(song);
+
+        public int GetPlayCountFor(BeatmapLevel song)
+        {
+            return song
+                .GetBeatmapKeys()
+                .Select(GetPlayCountForKey)
+                .Aggregate((a, b) => a + b);
+        }
+
+        IDictionary<BeatmapKey, PlayerLevelStatsData> PlayerLevelStatsData
+        {
+            get
+            {
+                if (playerDataModel == null)
+                    playerDataModel = UnityEngine.Object.FindObjectOfType<PlayerDataModel>();
+
+                return playerDataModel?.playerData?.levelsStatsData;
+            }
+        }
+        int GetPlayCountForKey(BeatmapKey key)
+        {
+            if (PlayerLevelStatsData == null)
+                return 0;
+
+            if (!PlayerLevelStatsData.TryGetValue(key, out var level))
+                return 0;
+
+            return level.playCount;
+        }
+
+        public Task Prepare(CancellationToken cancelToken) => Task.CompletedTask;
+        public void ContextSwitch(SelectLevelCategoryViewController.LevelCategory levelCategory, BeatmapLevelPack playlist) { }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BetterSongList;
+using IPA;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using IPALogger = IPA.Logging.Logger;
+
+[assembly: AssemblyTitle("BetterSongList PlayCount Sort")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyCopyright("MIT License - Copyright © 2024 Steffan Donal")]
+
+[assembly: Guid("f1378c34-e815-4e04-9471-8be443b18e96")]
+
+namespace PlayCount
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        static PlayCountSorter PlayCountSorter => new PlayCountSorter();
+
+        internal static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        public static readonly string Name = Assembly.GetCustomAttribute<AssemblyTitleAttribute>()?.Title;
+        public static readonly string Version = Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
+
+        public static IPALogger Log { get; internal set; }
+
+
+        static bool _isInitialized;
+
+        [Init]
+        public void Init(object _, IPALogger log)
+        {
+            Log = log;
+
+            SortMethods.RegisterPrimitiveSorter(PlayCountSorter);
+        }
+
+        [OnStart]
+        public void OnStart()
+        {
+            if (_isInitialized)
+                throw new InvalidOperationException($"Plugin had {nameof(OnStart)} called more than once! Critical failure.");
+
+            _isInitialized = true;
+
+            Log.Info($"v{Version} loaded!");
+        }
+    }
+}
